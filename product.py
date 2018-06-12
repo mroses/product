6e1e971ac70e7b1e4d01d5a95b72ba30e656683f
Marta <marta.rose@gmail.com>
2018-06-12 13:32:57 -0700

    updated product.py with version submitted

--- product.py
+++ product.py
@@ -5,15 +5,17 @@ class Product(object):
         self.brand = brand
         self.status = "for sale"
         self.price = price
-        self.display_info()
+        #self. reason = reason
+
+        #self.display_info()
         
     def sell(self):
         self.status = "sold"
         return self
 
     def add_tax(self, tax):
-            return self.price * (1 + tax)
-    
+        return self.price * (1 + tax)
+
     def return_reason(self, reason):
         #return_reason = None
         if reason.lower() == "defective":
@@ -21,9 +23,9 @@ class Product(object):
             self.status = "defective"
         elif reason.lower() == "like new":
             self.status = "for sale"
-        elif reason.lower() == "open":
+        elif reason.lower() == "opened":
             self.status = "used"
-            self.price = price * 0.10
+            self.price = self.price * 0.8
         return self
 
     def display_info(self):
@@ -35,13 +37,16 @@ class Product(object):
         return self
 
 product1 = Product("speakers", "3 lbs", "Bose", 50)
-print product1.add_tax(0.12)  
-product1.sell()
+product1.add_tax(0.12)  
+product1.return_reason("defective")
 product1.display_info()
-product1.return_reason("like new")
-product1.dispaly_info()
-
-
-#product1.sell()
-#product1.add_tax()
-#product1.dispaly_info()
+print " "
+product2 = Product("shovel", "5lbs", "Garden Magic", 20)
+product2.add_tax(.08)
+product2.return_reason("opened")
+product2.display_info()
+print " "
+product3 = Product("trampoline", "100lbs", "Jump Higher", 450)
+product3.add_tax(.07)
+product3.sell()
+product3.display_info()
